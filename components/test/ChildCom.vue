4fcbf79b3bbae5c35da5c4b07f4c586f6660f25a
than phourac <thanphourac@thans-MacBook-Pro.local>
2025-05-29 14:57:35 +0700

    update

--- components/test/ChildCom.vue
+++ components/test/ChildCom.vue
@@ -1,11 +1,13 @@
 <template>
-  <h1>{{ name }}</h1>
-  <h1>{{ props.name }}</h1>
-  <h1>{{ counter }}</h1>
+  <!-- <h1>{{ name }}</h1> -->
+  <!-- <h1>{{ props.name }}</h1> -->
+  <!-- <h1>{{ counter }}</h1> -->
 
-  <button @click="props.handleCLick">click</button>
+  <!-- <button @click="props.handleCLick">click</button> -->
   <slot name="header" />
-  <button @click="changeProps">chnage</button>
+  <!-- <button @click="changeProps">chnage</button> -->
+  <p>{{ props.listRef?.length! > 0 ? 'Yes' : 'No' }}</p>
+  <p>{{ JSON.stringify(props.listRef) }}</p>
 </template>
 
 <script setup lang="ts">
@@ -13,6 +15,7 @@ const props = defineProps<{
   name: string
   check?: boolean
   handleCLick: () => void
+  listRef?: { name: string; sex: string; phone: string }[]
 }>()
 
 import { ref } from 'vue'
@@ -22,6 +25,10 @@ import { useCounter } from '../../composables/useGlobalState'
 const { counter } = useCounter()
 
 const name = ref(props.name)
+
+const listComputed = computed(() => {
+  props.listRef?.length! > 0 ? 'Yes' : 'No'
+})
 
 const changeProps = () => {
   name.value = 'testing'
--- pages/test/index.vue
+++ pages/test/index.vue
@@ -1,7 +1,8 @@
 <template>
-  <ChildCom :name="refName" :handle-c-lick="handleCLick">
+  <ChildCom :name="refName" :handle-c-lick="handleCLick" :listRef="listRef">
     <template #header><p>testing</p></template>
   </ChildCom>
+  <!-- <p>{{ JSON.stringify(listRef) }}</p> -->
 </template>
 
 <script setup lang="ts">
@@ -9,6 +10,11 @@ import { ref } from 'vue'
 import ChildCom from '~/components/test/ChildCom.vue'
 
 const refName = ref('hello')
+
+const listRef = ref<{ name: string; sex: string; phone: string }[]>([
+  { name: 'test', phone: '098765432', sex: 'male' },
+  { name: 'test', phone: '098765432', sex: 'male' }
+])
 const handleCLick = () => {
   refName.value = 'baby'
 }
